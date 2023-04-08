1e47fa3c48fbc108d2a9b793c83d488d6f516d12
geniemo <kor98won@gmail.com>
2023-04-08 19:06:44 +0900

    init: 프로젝트 생성 및 기초 라이브러리 추가
    - react-router-dom -> 차트 별 페이지 구성
    - react-chartjs-2 -> 차트 그리기

---
+++ chartjs-practice/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes></Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
